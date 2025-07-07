ed5cda9348d34a62b200dfb3ce2e3bcf565ecd0a
Mateo Didier <didiermateo200@gmai.com>
2025-07-07 16:50:53 -0300

    me wa morir

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: Reagitct.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
