20144b5f93dee0e492f975449f358e674b6c404a
Mateo Didier <didiermateo200@gmai.com>
2025-07-07 17:51:10 -0300

    ultimo

--- app/productos/[slug]/page.tsx
+++ app/productos/[slug]/page.tsx
@@ -14,12 +14,17 @@ export async function generateStaticParams() {
   return productos.map((p) => ({ slug: p.slug }))
 }
 
-export default async function ProductPage({ params }: ProductPageProps) {
-  const { slug } = params
+
+export default async function ProductPage(props: Promise<ProductPageProps >) {
+
+  const resolvedProps = await props // resolvedProps tiene la forma { params: { slug: string } }
+  
+  // Ahora extraemos slug
+  const { slug } = resolvedProps.params
 
   const producto = await prisma.productos.findFirst({
     where: {
-      slug,
+      slug: slug,
       producto_base_id: null,
     },
     include: {
@@ -39,24 +44,22 @@ export default async function ProductPage({ params }: ProductPageProps) {
   }
 
   return (
-    <ProductPageClient
-      producto={{
-        id: producto.id,
-        nombre: producto.nombre,
-        imagen_principal: producto.imagen_principal,
-        descripcion: producto.descripcion,
-        precio_base: Number(producto.precio_base),
-        categorias: producto.categorias
-          ? { nombre: producto.categorias.nombre }
-          : { nombre: "Sin categoría" }, // asegurate de cumplir el tipo
-        other_productos: producto.other_productos
-          .filter((p) => p.talle && p.color_nombre)
-          .map((p) => ({
-            talle: p.talle as string,
-            color_nombre: p.color_nombre as string,
-            stock: p.stock,
-          })),
-      }}
-    />
-  )
+      <ProductPageClient
+        producto={{
+              id: producto.id,
+              nombre: producto.nombre,
+              imagen_principal: producto.imagen_principal,
+              descripcion: producto.descripcion,
+              precio_base: Number(producto.precio_base),
+              categorias: producto.categorias ? { nombre: producto.categorias.nombre } : null,
+              other_productos: producto.other_productos
+                .filter((p) => p.talle && p.color_nombre) // <--- esto es clave
+                .map((p) => ({
+                  talle: p.talle as string,
+                  color_nombre: p.color_nombre as string,
+                  stock: p.stock,
+                })),
+            }}
+      />
+    )
 }
